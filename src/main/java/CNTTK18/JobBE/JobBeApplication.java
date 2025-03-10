642b0d96711ea51769fb0cd560204b4e0f11c324
buihuy13 <buiquochuytd@gmail.com>
2025-03-10 23:58:29 +0700

    init code

---
+++ src/main/java/CNTTK18/JobBE/JobBeApplication.java
@@ -0,0 +1,12 @@
+package CNTTK18.JobBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBeApplication.class, args);
+	}
+}
---
+++ src/test/java/CNTTK18/JobBE/JobBeApplicationTests.java
@@ -0,0 +1,13 @@
+package CNTTK18.JobBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
