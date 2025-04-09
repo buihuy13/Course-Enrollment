f18805a539d48c38a1953f68f64900342d754578
buihuy13 <buiquochuytd@gmail.com>
2025-04-09 19:38:06 +0700

    add authority to lophoc

--- src/main/java/CNTTK18/JobBE/Controllers/ClassController.java
+++ src/main/java/CNTTK18/JobBE/Controllers/ClassController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import CNTTK18.JobBE.Services.ClassService;
 
 @RestController
 @RequestMapping("/classes")
+@PreAuthorize("hasRole('ADMIN')")
 public class ClassController {
     private final ClassService classService;
 
